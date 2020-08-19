f206971cee3e58566427e21079a0ecb0af986a5b
nzhy <69903677+nzhy@users.noreply.github.com>
2020-08-19 19:11:34 +0700

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,30 @@
+/*
+ * xTime - Responsive Coming Soon Template
+ *
+ * Author: www.matchthemes.com
+ *
+ */
+ 
+jQuery(document).ready(function($) {
+ 
+
+	"use strict";
+	
+	
+	  $('#countdown').countDown({
+	        targetDate: {
+	            'day':      25,
+          		'month':    12,
+	            'year':     2015,
+	            'hour':     0,
+	            'min':      0,
+    	        'sec':      0
+	        },
+        omitWeeks: true
+	    });
+
+})
+
+
+
+
